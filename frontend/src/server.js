3a395e38aded23b6266cb30b57457df198ac3442
adilarain00 <addilarain00@gmail.com>
2025-02-04 16:52:07 +0500

    update links about frontend and backend

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,5 +1,4 @@
-export const server = "https://mern-e-shop.vercel.app/api/v2";
-
-export const backend_url = "https://mern-e-shop.vercel.app/"
+export const server = "https://mern-e-shop-backend.vercel.app//api/v2";
+export const backend_url = "https://mern-e-shop-backend.vercel.app/";
 
 
